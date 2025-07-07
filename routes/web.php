ae1fff684864f3496b62e5d9569fa9f5a65ab8af
sandrine-bd <sandrine.barrucand@gmail.com>
2025-07-07 14:08:29 +0200

    ajout example-app

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Hello World From Laravel v12.x (PHP v8.x.x)';
+    return 'Homepage';
+});
+
+Route::get('/product', function () {
+    return 'Liste des produits';
+});
+
+Route::get('/product/{id}', function ($id) {
+    return 'Fiche du produit {id}';
+});
+
+Route::get('/cart', function () {
+    return 'Panier';
 });
