8ba1356e9085b1df91b08beb02e32d4ee1e7d31c
sandrine-bd <sandrine.barrucand@gmail.com>
2025-07-17 12:21:58 +0200

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,38 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'show']);
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'show'])->name('home');
+
+Route::get('/products', [\App\Http\Controllers\ProductController::class, 'index'])->name('products');
+
+Route::get('/products/{id}', [\App\Http\Controllers\ProductController::class, 'showProduct'])->name('products.details');
+
+Route::get('/selling', [\App\Http\Controllers\SellingController::class, 'show']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/panier', [\App\Http\Controllers\CartController::class, 'index'])->name('cart.index');
+    Route::post('/panier/ajouter/{id}', [\App\Http\Controllers\CartController::class, 'add'])->name('cart.add');
+    Route::delete('/panier/supprimer/{id}', [\App\Http\Controllers\CartController::class, 'remove'])->name('cart.remove');
+    Route::delete('/panier/vider', [\App\Http\Controllers\CartController::class, 'clear'])->name('cart.clear');
 });
+
+Route::get('/account', [\App\Http\Controllers\AccountController::class, 'show']);
+
+Route::get('/backoffice', [\App\Http\Controllers\Backoffice\ProductController::class, 'index'])->name('backoffice');
+
+Route::resource('/backoffice/products', \App\Http\Controllers\Backoffice\ProductController::class, [
+    'names' => [
+        'index' => 'products.index',
+        'create' => 'products.create',
+        'store' => 'products.store',
+        'show' => 'products.show',
+        'edit' => 'products.edit',
+        'update' => 'products.update',
+        'destroy' => 'products.destroy',
+    ]
+]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
