bfb59e70de98e673d3cae38ea459cf183c9a86b4
Sandrine Barrucand <sandrine.barrucand@gmail.com>
2025-07-21 09:59:13 +0200

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -53,7 +53,6 @@ class ProfileController extends Controller
         $user->delete();
 
         $request->session()->invalidate();
-        $request->session()->regenerateToken();
 
         return Redirect::to('/');
     }
